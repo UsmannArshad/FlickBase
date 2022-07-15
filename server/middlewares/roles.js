a532726c18c753406064313b3a79e58dd1e45f4f
UsmannArshad <usmanarshadbhatti480@gmail.com>
2022-07-15 09:37:38 +0500

    read,update,delete article

--- server/middlewares/roles.js
+++ server/middlewares/roles.js
@@ -3,11 +3,7 @@ exports.CheckPermission=function(action,resource){
     return async(req,res,next)=>
     {
         try{
-            console.log(req.user.role)
-            console.log(action)
-            console.log(resource)
             const permission=roles.can(req.user.role)[action](resource)
-            console.log(permission)
             if(!permission.granted)
             {
                 return res.status(400).send('Permission not granted')
--- server/routes/article_route.js
+++ server/routes/article_route.js
@@ -2,7 +2,8 @@ const Express=require('express');
 const {article} = require('../models/article_model');
 const Router=Express.Router();
 const {checkLoggedIn} =require('../middlewares/auth')
-const {CheckPermission}=require('../middlewares/roles')
+const {CheckPermission}=require('../middlewares/roles');
+const mongoose=require('mongoose')
 Router.route('/addarticle')
 .post(checkLoggedIn,CheckPermission('createAny','article'),async(req,res)=>{
     try{
@@ -16,6 +17,52 @@ Router.route('/addarticle')
     {
         console.log(error)
         res.status(400).json({message:"error while posting new article",error:error})
+    }
+})
+Router.route('/:id')
+.get(checkLoggedIn,CheckPermission('readAny','article'),async(req,res)=>{
+    try{
+        const id= mongoose.Types.ObjectId(req.params.id.trim());
+        const article1=await article.findById(id)
+        if(!article1||article1.length===0) return res.status(400).send("Article not found")
+        res.status(200).send(article1)
+    }
+    catch(error)
+    {
+        console.log(error)
+        res.status(400).json({message:"error while displaying article",error:error})
+    }
+})
+.patch(checkLoggedIn,CheckPermission('updateAny','article'),async(req,res)=>{
+    try{
+        const id=mongoose.Types.ObjectId(req.params.id.trim())
+        const article1=await article.findByIdAndUpdate(
+            id,
+            {$set:{
+                ...req.body
+            }},
+            {new:true}
+        )
+        if(!article1) return res.status(400).send('Article not found')
+        res.status(200).send(article1)
+    }
+    catch(error)
+    {
+        console.log(error)
+        res.status(400).json({message:"error while displaying article",error:error})
+    }
+})
+.delete(checkLoggedIn,CheckPermission('deleteAny','article'),async(req,res)=>{
+    try{
+        const id=mongoose.Types.ObjectId(req.params.id.trim())
+        const article1=await article.findByIdAndRemove(id)
+        if(!article1) res.status(400).send('User not found')
+        res.status(200).send("Deleted Succesfully")
+    }
+    catch(error)
+    {
+        console.log(error)
+        res.status(400),json({message:'Error Deleting',error:error})
     }
 })
 module.exports=Router
